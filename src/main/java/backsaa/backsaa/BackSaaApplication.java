77a2954a0aa0c61d7976084d3f52f6ae559643ab
Michael Silva <michaelsilvapp@gmail.com>
2017-10-31 20:14:23 -0200

    SAA -01 Criação do projeto.

---
+++ src/main/java/backsaa/backsaa/BackSaaApplication.java
@@ -0,0 +1,12 @@
+package backsaa.backsaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackSaaApplication.class, args);
+	}
+}
